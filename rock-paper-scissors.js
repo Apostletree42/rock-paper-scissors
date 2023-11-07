d045db44e878b112d42dfba58a540ac0466659cf
treegod <113334799+Apostletree42@users.noreply.github.com>
2023-11-07 19:11:13 +0530

    ✨ 💄 ♻️ Multiple commits
    Did multiple things in this commit, including:
        -Added buttons for game
        -modified js code to play with buttons
        -styling for html elements
    Just this once. I won't do this again and will commit responsibly in the
    future.
    
    Author: Tree God
    Date: 07/11/2023

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,14 @@
-const button = document.getElementById('button1')
+const button = document.getElementById('button1');
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+const feedback1 = document.querySelector('#feedback1');
+const feedback2 = document.querySelector('#feedback2');
+let playerScore = 0;
+let computerScore = 0;
 
 button.addEventListener('click', function() {
+    feedback1.innerHTML = 'Select below to play :D';
     function getComputerChoice(){
         let randomNumber = Math.floor(Math.random() * 3);
         switch(randomNumber){
@@ -9,27 +17,59 @@ button.addEventListener('click', function() {
             case 2: return 'scissors';
         }
     }
-    function playRound(playerChoice, computerChoice) {
-        if (playerChoice == computerChoice){
-            return "It's a tie!";
+
+    function playGame(playerChoice) {
+        const computerChoice = getComputerChoice();
+        let roundResult = '';
+
+        if (playerChoice === computerChoice) {
+            roundResult = "It's a tie!";
+        } 
+        else if ((playerChoice == 'rock' && computerChoice == 'scissors') || 
+                 (playerChoice == 'paper' && computerChoice == 'rock') || 
+                 (playerChoice == 'scissors' && computerChoice == 'paper')) {
+            roundResult = "You won this round!";
+            playerScore++;
+        } 
+        else {
+            roundResult = "Computer won this round!";
+            computerScore++;
         }
-        if (playerChoice == 'rock'){
-            return (computerChoice == 'paper') ? "You have lost this match." : "You won this match!";
-        }
-        else if (playerChoice == 'paper'){
-            return (computerChoice == 'rock') ? "You won this match!" : "You have lost this match.";
-        }
-        else if (playerChoice == 'scissors' || playerChoice == 'scissor') {
-            return (computerChoice == 'rock') ? "You have lost this match." : "You have won this match!";
+
+        //feedback2.innerHTML = roundResult;
+        setTimeout(() => {
+            feedback2.innerHTML = roundResult;
+            feedback2.style.opacity = '1';
+        }, 300);
+        feedback1.innerHTML = `Player = ${playerScore} - Computer = ${computerScore}`;
+
+        if (playerScore >= 5 || computerScore >= 5){
+            endGame();
         }
     }
-    function game() {
-        for (let i = 0; i < 5; i++){
-            const playerChoice = prompt("Will you choose rock, paper or scissors?\n").toLowerCase();
-            const computerChoice = getComputerChoice();
-            console.log(playRound(playerChoice, computerChoice));
+
+    function endGame() {
+        if (playerScore >= 5) {
+            feedback1.innerHTML = "Player wins the game!";
+        } else {
+            feedback1.innerHTML = "Computer wins the game!";
         }
+        // Disable the buttons and reset the scores or perform other end-game actions if necessary
+        rock.disabled = true;
+        paper.disabled = true;
+        scissors.disabled = true;
+        button.disabled = true;
     }
-    game();
-}
-);
+
+    rock.addEventListener('click', function(){
+        playGame('rock');
+    });
+
+    paper.addEventListener('click', function(){
+        playGame('paper');
+    });
+
+    scissors.addEventListener('click', function(){
+        playGame('scissors');
+    });
+});
